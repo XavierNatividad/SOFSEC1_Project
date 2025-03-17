52f3425cf98c738dd24d6d48946ab7b57d8ea643
Xavier Natividad <xavier.natividad@gmail.com>
2025-03-17 20:21:02 +0800

    created GradeModel, ProgramModel, User_LoginModel, and User_ProfileModel

---
+++ SOFSEC1_Project/ProgramModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOFSEC1_Project
+{
+    class ProgramModel
+    {
+        public int programId { get; set; }
+        public string programCode { get; set; }
+        public string programName { get; set; }
+    }
+}
---
+++ SOFSEC1_Project/User_ProfileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOFSEC1_Project
+{
+    class User_ProfileModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string program { get; set; }
+    }
+}
